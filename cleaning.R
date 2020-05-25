36e43c5d4302f64df4aa293523bc30e04043ac32
minkim2020 <minkim2020@u.northwestern.edu>
2020-05-24 17:23:39 -0700

    test

---
+++ cleaning.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(skimr)
+library(RColorBrewer)
+
+
+# price history -----------------------------------------------------------
+# source: https://www.kaggle.com/philmohun/cryptocurrency-financial-data
