92d1cac807cfd52d3dfc45bd030402ce2a40029f
minkim2020 <minkim2020@u.northwestern.edu>
2020-05-24 18:19:44 -0700

    historical prices and initial graphs

--- cleaning.R
+++ cleaning.R
@@ -1,7 +1,10 @@
 library(tidyverse)
+library(janitor)
 library(skimr)
 library(RColorBrewer)
 
 
 # price history -----------------------------------------------------------
 # source: https://www.kaggle.com/philmohun/cryptocurrency-financial-data
+
+read_csv("data/consolidated_coin_data.csv")
