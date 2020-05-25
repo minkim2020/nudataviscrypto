6d7538033ccd31c36f3ef76a99374fa766421d3c
minkim2020 <minkim2020@u.northwestern.edu>
2020-05-24 18:57:25 -0700

    restore cleaning file

---
+++ cleaning.R
@@ -0,0 +1,46 @@
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+library(janitor)
+library(skimr)
+library(lubridate)
+library(RColorBrewer)
+
+
+# Historical Prices -------------------------------------------------------
+# source: https://www.kaggle.com/philmohun/cryptocurrency-financial-data
+
+price_dat <- read_csv("data_unprocessed/consolidated_coin_data.csv")
+
+price_dat <- price_dat %>%
+  rename(slug = Currency,
+         date = Date,
+         open = Open,
+         high = High,
+         low = Low,
+         close = Close,
+         volume = Volume,
+         market_cap = `Market Cap`)
+
+price_dat$date <- mdy(price_dat$date)
+
+
+# Names -------------------------------------------------------------------
+# source: https://www.kaggle.com/afriblossom/crypto
+names <- read_csv("data_unprocessed/crypto-markets.csv") %>%
+  select(slug, symbol, name)
+
+price_dat <- left_join(price_dat, names, by = "slug") %>% distinct()
+
+price_dat <- price_dat %>%
+  mutate(name = case_when(slug == "xrp" ~ "Ripple",
+                          slug == "bitcoin-sv" ~ "Bitcoin SV",
+                          TRUE ~ name),
+         symbol = case_when(slug == "xrp" ~ "XRP",
+                            slug == "bitcoin-sv" ~ "BSV",
+                            TRUE ~ symbol))
+
+write_csv(price_dat, "data_processed/price_dat")
+
+# Awareness ---------------------------------------------------------------
+# maybe: https://www.kaggle.com/kashnitsky/news-about-major-cryptocurrencies-20132018-40k
+# google mentions?
--- graph_drafts.R
+++ graph_drafts.R
@@ -6,11 +6,14 @@ library(skimr)
 library(lubridate)
 library(RColorBrewer)
 
+<<<<<<< HEAD
 
 # data --------------------------------------------------------------------
 price_dat <- read_csv("data_processed/price_dat.csv")
 
 
+=======
+>>>>>>> 2ec3f19aecfd641b3d3c1b00095887eab455fa25
 # plots (test) ------------------------------------------------------------
 theme_global <- theme_minimal() + 
   theme(plot.title = element_text(hjust = 0.5, family = "Arial", face = "bold", size = 13),
