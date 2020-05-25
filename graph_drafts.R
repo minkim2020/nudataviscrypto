4e6a4adeb377dd505152634d55104d3fcf5fff85
minkim2020 <minkim2020@u.northwestern.edu>
2020-05-24 18:19:44 -0700

    historical prices

---
+++ graph_drafts.R
@@ -0,0 +1,32 @@
+
+# Packages ----------------------------------------------------------------
+library(tidyverse)
+library(janitor)
+library(skimr)
+library(lubridate)
+library(RColorBrewer)
+
+
+# data --------------------------------------------------------------------
+price_dat <- read_csv("data_processed/price_dat.csv")
+
+
+# plots (test) ------------------------------------------------------------
+theme_global <- theme_minimal() + 
+  theme(plot.title = element_text(hjust = 0.5, family = "Arial", face = "bold", size = 13),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        axis.text.x = element_text(angle = 70, hjust = 1),
+        axis.text = element_text(family = "Arial", size = 7),
+        axis.title = element_text(family = "Arial", size = 10, face = "bold"),
+        legend.text = element_text(family = "Arial", size = 7),
+        legend.title = element_text(family = "Arial", size = 10))
+
+
+price_dat %>%
+  ggplot(aes(x = date, y = market_cap, color = symbol)) +
+  geom_line() + 
+  ggtitle("Historical Market Cap") +
+  labs(x = "Date", y = "Market Cap") +
+  theme_global
+
